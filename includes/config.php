ac9e790d0faa04aa62646234410e42c8f8514992
namoclopez2020 <62824677+namoclopez2020@users.noreply.github.com>
2020-09-11 13:20:10 -0500

    Eliminados archivos no deseados

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  date_default_timezone_set('America/Lima');
-  define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'jose' );             // Set database user
-  define( 'DB_PASS', 'jose' );             // Set database password
-  define( 'DB_NAME', 'mrrobot' );        // Set database name
-
-?>
